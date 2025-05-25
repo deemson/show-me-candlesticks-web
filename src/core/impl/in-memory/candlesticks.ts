202063045d26f5a6648cc23ebbaaad3552fd41a4
Kurkin <deemson.k@gmail.com>
2025-05-25 19:44:24 +0200

    re-implementation of in-memory cache

--- src/core/impl/in-memory/candlesticks.ts
+++ src/core/impl/in-memory/candlesticks.ts
@@ -1,92 +1,42 @@
 import type { Candlestick } from "@/core/base/candlesticks";
-import type { CacheStore as ICacheStore } from "@/core/util/candlesticks";
-import type { Interval } from "@/core/base/interval";
-import * as intervalLib from "@/core/base/interval";
-import { UTCDate } from "@date-fns/utc";
+import type { Store as IBlockCacheStore, BlockMap } from "@/core/impl/cache/candlesticks/block-io";
+import { Mutex } from "async-mutex";
 
-export class FixedBlockSizeCacheStore implements ICacheStore {
-  private readonly blockSize: number;
-  private readonly store: Map<number, Candlestick[]>;
+export type BrowserConsoleCache = BrowserConsoleCacheItem[];
 
-  constructor(
-    private readonly interval: Interval,
-    blockSize: number,
-  ) {
-    this.blockSize = Math.floor(blockSize);
-    this.store = new Map<number, Candlestick[]>();
+export class BrowserConsoleCacheItem {
+  constructor(readonly block: Candlestick[]) {}
+
+  valueOf() {
+    return "ssssup";
+  }
+}
+
+export class ChromeDevConsoleReadyStore implements IBlockCacheStore {
+  private readonly mutex: Mutex;
+
+  constructor(private readonly cache: BrowserConsoleCache) {
+    this.mutex = new Mutex();
   }
 
-  async load(fromTimestamp: number, toTimestamp: number): Promise<Candlestick[][]> {
-    const fromBlock = this.timestampToBlockNumber(fromTimestamp);
-    const toBlock = this.timestampToBlockNumber(toTimestamp);
-    const candlestickBlocks: Candlestick[][] = [];
-    if (this.store.has(fromBlock)) {
-      const firstBlock = this.store.get(fromBlock) as Candlestick[];
-      for (let i = 0; i < firstBlock.length; i++) {
-        if (firstBlock[i].timestamp >= fromTimestamp) {
-          candlestickBlocks.push(firstBlock.slice(i));
-          break;
+  async load(fromBlockNumber: number, toBlockNumber: number): Promise<BlockMap> {
+    return await this.mutex.runExclusive(async () => {
+      const blockMap: BlockMap = new Map();
+      for (let i = fromBlockNumber; i <= toBlockNumber; i++) {
+        const cacheItem = this.cache[i];
+        if (cacheItem !== undefined) {
+          blockMap.set(i, cacheItem.block);
         }
       }
-    } else {
-      candlestickBlocks.push([]);
-    }
-    for (let i = fromBlock + 1; i < toBlock; i++) {
-      if (!this.store.has(i)) {
-        if (candlestickBlocks[candlestickBlocks.length - 1].length > 0) {
-          candlestickBlocks.push([]);
-        }
-        continue;
-      }
-      const candlestickBlock = this.store.get(i) as Candlestick[];
-      candlestickBlocks[candlestickBlocks.length - 1] = [
-        ...candlestickBlocks[candlestickBlocks.length - 1],
-        ...candlestickBlock,
-      ];
-    }
-    if (this.store.has(toBlock)) {
-      const lastBlock = this.store.get(toBlock) as Candlestick[];
-      for (let i = 0; i < lastBlock.length; i++) {
-        if (lastBlock[i].timestamp === toTimestamp) {
-          candlestickBlocks[candlestickBlocks.length - 1] = [
-            ...candlestickBlocks[candlestickBlocks.length - 1],
-            ...lastBlock.slice(0, i + 1),
-          ];
-          break;
-        }
-        if (lastBlock[i].timestamp > toTimestamp) {
-          candlestickBlocks[candlestickBlocks.length - 1] = [
-            ...candlestickBlocks[candlestickBlocks.length - 1],
-            ...lastBlock.slice(0, i),
-          ];
-          break;
-        }
-      }
-    }
-    if (candlestickBlocks[candlestickBlocks.length - 1].length === 0) {
-      return candlestickBlocks.slice(0, -1);
-    }
-    return candlestickBlocks;
+      return blockMap;
+    });
   }
 
-  async save(candlesticks: Candlestick[]): Promise<void> {
-    const updateMap = new Map<number, Candlestick[]>();
-    for (const candlestick of candlesticks) {
-      const blockNumber = this.timestampToBlockNumber(candlestick.timestamp);
-      if (!updateMap.has(blockNumber)) {
-        updateMap.set(blockNumber, []);
+  async save(blockMap: BlockMap): Promise<void> {
+    await this.mutex.runExclusive(async () => {
+      for (const [i, v] of blockMap.entries()) {
+        this.cache[i] = new BrowserConsoleCacheItem(v);
       }
-      updateMap.get(blockNumber)?.push(candlestick);
-    }
-    for (const [key, value] of updateMap) {
-      if (value.length === this.blockSize) {
-        this.store.set(key, value);
-      }
-    }
-  }
-
-  private timestampToBlockNumber(timestamp: number): number {
-    const n = intervalLib.numberSinceEpoch(this.interval, new UTCDate(timestamp));
-    return Math.floor(n / this.blockSize);
+    });
   }
 }
