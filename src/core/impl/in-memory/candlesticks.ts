772914e959bd6b77133d98ff6fbfb034ddbe46fc
Kurkin <deemson.k@gmail.com>
2025-05-26 11:34:49 +0200

    in-mem cache is nice to view in the console

--- src/core/impl/in-memory/candlesticks.ts
+++ src/core/impl/in-memory/candlesticks.ts
@@ -1,18 +1,25 @@
 import type { Candlestick } from "@/core/base/candlesticks";
 import type { Store as IBlockCacheStore, BlockMap } from "@/core/impl/cache/candlesticks/block-io";
+import { UTCDate } from "@date-fns/utc";
 import { Mutex } from "async-mutex";
 
 export type BrowserConsoleCache = BrowserConsoleCacheItem[];
 
-export class BrowserConsoleCacheItem {
-  constructor(readonly block: Candlestick[]) {}
-
-  valueOf() {
-    return "ssssup";
-  }
+export interface BrowserConsoleCacheItem {
+  range: string;
+  candlesticks: Candlestick[];
 }
 
-export class ChromeDevConsoleReadyStore implements IBlockCacheStore {
+const cs2cis = (candlesticks: Candlestick[]): BrowserConsoleCacheItem => {
+  const from = new UTCDate(candlesticks[0].timestamp).toISOString();
+  const to = new UTCDate((candlesticks.at(-1) as Candlestick).timestamp).toISOString();
+  return {
+    range: `${from} - ${to}`,
+    candlesticks: candlesticks,
+  };
+};
+
+export class BrowserConsoleCacheStore implements IBlockCacheStore {
   private readonly mutex: Mutex;
 
   constructor(private readonly cache: BrowserConsoleCache) {
@@ -25,7 +32,7 @@ export class ChromeDevConsoleReadyStore implements IBlockCacheStore {
       for (let i = fromBlockNumber; i <= toBlockNumber; i++) {
         const cacheItem = this.cache[i];
         if (cacheItem !== undefined) {
-          blockMap.set(i, cacheItem.block);
+          blockMap.set(i, cacheItem.candlesticks);
         }
       }
       return blockMap;
@@ -35,7 +42,7 @@ export class ChromeDevConsoleReadyStore implements IBlockCacheStore {
   async save(blockMap: BlockMap): Promise<void> {
     await this.mutex.runExclusive(async () => {
       for (const [i, v] of blockMap.entries()) {
-        this.cache[i] = new BrowserConsoleCacheItem(v);
+        this.cache[i] = cs2cis(v);
       }
     });
   }
